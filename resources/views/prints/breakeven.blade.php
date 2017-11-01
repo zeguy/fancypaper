0e45984d12b31f97089fef437297a3d8d97d3763
zeguy <smullan.csheep@gmail.com>
2017-11-01 18:36:50 -0400

    w3validator

--- resources/views/prints/breakeven.blade.php
+++ resources/views/prints/breakeven.blade.php
@@ -25,15 +25,16 @@
 
 	</fieldset>
 </form>
-
-@if(count($errors) > 0)
-	<ul>
-		@foreach ($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-	</ul>
-@else
-	{{ $message }}
-@endif
+<div>
+	@if(count($errors) > 0)
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	@else
+		{{ $message }}
+	@endif
+</div>
 
 @endsection
